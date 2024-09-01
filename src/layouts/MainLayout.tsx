01b716f00a50b9850b179957c05729839b31fc61
pok9 <pokhappy1999@gmail.com>
2024-09-01 13:14:23 +0700

    update

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,11 +5,9 @@ import Sidebar from '../components/specific/Sidebar/Sidebar'
 export default function MainLayout() {
   return (
     <div className={styles.container}>
-      <div style={{ position: "relative"}}>
         <div className={styles.sidebar}>
           <Sidebar />
         </div>
-      </div>
       <div className={styles.mainContent}>
         <Outlet />
       </div>
